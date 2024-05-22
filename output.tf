b4ab400873f14cbb4be537352784a9b4bc9cd012
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-05-22 17:22:16 +0530

    module user variables given

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 # output "azs_info" {
 #     value = module.vpc_test.availability_zones #module.module_name.<output>
 # }
+
+output "vpc_id" {
+    value = module.vpc_test.vpc_id #catching the output from module 
+}
+output "public_subnet_list" {
+    value = module.vpc_test.public_subnet_ids
+}
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,10 @@
 module "vpc_test" {
-  source = "../terraform-aws-vpc"
+  # source = "../terraform-aws-vpc"
+  source = "git::https://github.com/NagamaniPalemkota/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   common_tags = var.common_tags
   public_subnet_cidrs = var.public_sub_cidrs
   private_subnet_cidrs = var.private_sub_cidrs
   database_subnet_cidrs = var.database_sub_cidrs
+  is_peering_required = var.is_peering_required
 }
